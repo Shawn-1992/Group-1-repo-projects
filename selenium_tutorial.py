5b0390a51e467e3c9ee6e1954fe2ebf6e19c6249
Shawn-1992 <146864289+Shawn-1992@users.noreply.github.com>
2023-10-12 17:03:32 -0400

    Update selenium_tutorial.py
    Added comments

--- selenium_tutorial.py
+++ selenium_tutorial.py
@@ -1,3 +1,9 @@
+#The /search route: This route just displays a message about searching. You can use this to demonstrate URL parameters and query strings in your Selenium tutorial.
+
+#The /search/results/<query> route: This route takes a query parameter from the URL and displays the search results. This can be used to illustrate how to handle dynamic data in web pages.
+
+#The /form route: This route renders an HTML form and handles both GET and POST requests. It's used to show how to interact with forms on web pages. 
+#To make this work, you'll need to create an HTML template for the form (e.g., form.html) and store it in a folder named templates in the same directory as your Flask application.
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
