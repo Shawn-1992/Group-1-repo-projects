b2a0cc3f7a45fb85164bacb542462954da39e449
Shawn-1992 <146864289+Shawn-1992@users.noreply.github.com>
2023-10-12 17:09:59 -0400

    Update selenium_tutorial.py
    added ideas on how to use web application to teach selenium.

--- selenium_tutorial.py
+++ selenium_tutorial.py
@@ -1,3 +1,5 @@
+#This is just a simple web application built with Flask.
+#You will have to install Flask to run this code. 
 #The /search route: This route just displays a message about searching. You can use this to demonstrate URL parameters and query strings in your Selenium tutorial.
 
 #The /search/results/<query> route: This route takes a query parameter from the URL and displays the search results. This can be used to illustrate how to handle dynamic data in web pages.
@@ -33,3 +35,40 @@ def form():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+#IDEAS FOR SELENIUM TUTORIAL LAB
+    #Web Page Navigation:
+        #Use Selenium to navigate to different routes within the Flask application, such as the root URL, the search page, and the form page.
+
+    #Form Interaction:
+        #Demonstrate how Selenium can interact with HTML forms. Fill out the form fields and submit the form using Selenium automation.
+
+    #Element Identification:
+        #Show how to locate and interact with specific HTML elements on a web page, such as input fields, buttons, and links.
+
+    #Text Extraction and Validation:
+        #Use Selenium to extract text content from web pages and validate that the content matches expectations. For example, you can use Selenium to confirm that a specific message or result is displayed after form submission.
+
+    #Dynamic Content Handling:
+        #Showcase how Selenium can handle dynamic content and elements on web pages. For example, when you pass a dynamic query to a search results page, demonstrate how Selenium can locate and validate results based on the query parameter.
+
+    #Page Interaction and Navigation:
+        #Illustrate how Selenium can perform interactions with web pages, such as clicking links, buttons, or navigation between pages.
+
+    #Implicit and Explicit Waits:
+        #Explain the concept of waiting in Selenium and demonstrate the use of implicit and explicit waits to handle elements that may take time to load or become interactive.
+
+    #Taking Screenshots:
+        #Use Selenium to take screenshots of web pages at specific points during automation, which can be useful for debugging and reporting.
+
+    #Handling Pop-up Windows:
+        #If your application includes pop-up windows or dialogs, show how Selenium can handle them effectively.
+
+    #Testing Scenarios:
+        #Create test scenarios that involve multiple interactions and verifications. For example, a test scenario might involve navigating to a search page, performing a search, and validating search results.
+
+    #Browser Management:
+        #Showcase how Selenium can manage browsers, open multiple browser instances, or control browser settings.
+
+    #Cross-Browser Testing:
+        #If applicable, demonstrate how Selenium can be used for cross-browser testing by running the same tests on different web browsers.
