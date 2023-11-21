6e8a62d343d26b3a08cc98d2e516b32e559537ab
njgarner <118210681+njgarner@users.noreply.github.com>
2023-11-21 18:46:48 -0500

    Adding TDD template
    Template for the TDD lab challenge.

---
+++ tdd_template.py
@@ -0,0 +1,37 @@
+# import pytest
+# from unittest.mock import Mock
+# from typing import List
+
+# Ensure you use the following libraries listed above. Reference the Additional
+# Resources section of the documentation if you are unsure how any
+# of the libraries work.
+
+# This file will allow you to create a simple program following a TDD framework.
+# Resources on Pytest, Typing, and Unittest.mock libraries
+# are located in the documentation.
+
+# Below, you will create a minium of five instances of a failing test and then
+# implement the minimum amount of production code to pass the test before refactoring.
+
+# The step-by-step process is outlined for you. Examples below are for
+# you to alter in creating a program of similar purpose to the sample_tdd.
+
+# Failing Test
+
+# Production Code / Pytest Fixture
+
+# Failing Test
+
+# Production Code / Pytest Raises
+
+# Failing Test
+
+# Production Code
+
+# Failing Test
+
+# Production Code
+
+# Failing Test / Test using unittest.mock values
+
+# Production Code
