59150c8b60569fce10a87c0d0d6eac801d1d9c5b
njgarner <118210681+njgarner@users.noreply.github.com>
2023-11-21 18:48:15 -0500

    Adding TDD Lab
    The TDD lab file to be used alongside the TDD test file.

---
+++ tdd_lab.py
@@ -0,0 +1,18 @@
+# from typing import List
+
+# The following class was used in the sample_tdd file.
+class ShippingCrate:
+    def placeholder1(self):
+        pass
+
+# def placeholder2():
+#     pass
+
+# def placeholder3():
+#     pass
+
+# def placeholder4():
+#     pass
+
+# def placeholder5():
+#     pass
