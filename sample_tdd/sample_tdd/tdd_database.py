1f383c065e2ce3928fa02bcf5249aeaa85378194
Shawn-1992 <146864289+Shawn-1992@users.noreply.github.com>
2023-11-27 13:22:44 -0500

    Delete sample_tdd/sample_tdd directory

--- sample_tdd/sample_tdd/tdd_database.py
+++
@@ -1,8 +0,0 @@
-# Class that could potentially connect to a database to assign product cost.
-# Since there is no database, mock values are used to assign the costs.
-class ProductDatabase:
-    def __init__(self) -> None:
-        pass
-
-    def get(self, product: str) -> float:
-        pass
